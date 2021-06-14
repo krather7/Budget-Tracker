f44decd29cbf81262e92aa9496d4fc9a00d805e0
Kyle Rather <krather7@gmail.com>
2021-06-14 10:22:16 -0500

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -143,11 +143,17 @@ function sendTransaction(isAdding) {
     amountEl.value = "";
   });
 }
-
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").addEventListener("click", function(event) {
+  event.preventDefault();
   sendTransaction(true);
-};
+});
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector("#sub-btn").addEventListener("click", function(event) {
+  event.preventDefault();
   sendTransaction(false);
-};
+});
+
+document.querySelector("#del-btn").addEventListener("click", function(event) {
+  event.preventDefault();
+  deletePending();
+});
